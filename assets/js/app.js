be4b3602ff699fd840d714b411183491264501ee
Abisai Rodriguez <abisai.rodriguez13@gmail.com>
2019-06-03 15:13:11 -0700

    drop-down menu links functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,33 @@
 const burger = document.querySelector(".burger");
 const navLinks = document.querySelectorAll(".nav-links li");
+var nav = document.querySelector(".nav-links");
 
-
-burger.addEventListener("click", () => {
-    // toggle nav
-    document.querySelector(".nav-links").classList.toggle("active");
-    // animate links
-    navLinks.forEach( (link, index) => {
-        if(link.style.animation){
-            link.style.animation = "";
-        }
-        else {
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-        }
-
-        console.log(link.style.animation);
+    burger.addEventListener("click", () => {
+        // toggle nav
+        nav.classList.toggle("active");
+        // animate links
+        navLinks.forEach( (link, index) => {
+            if(link.style.animation){
+                link.style.animation = "";
+            }
+            else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+        //burger animation
+        burger.classList.toggle("toggle");
     });
-    //burger animation
-    burger.classList.toggle("toggle");
-});
 
-const app = () => {
-    burger();
-}
+    //reset on clicking nav links
+    nav.addEventListener("click", () =>{
+        burger.classList.remove("toggle");
+        nav.classList.remove("active");
+        navLinks.forEach( (link, index) => {
+            if(link.style.animation){
+                link.style.animation = "";
+            }
+            else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        })
+    })
